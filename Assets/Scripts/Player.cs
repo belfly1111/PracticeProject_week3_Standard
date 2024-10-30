b6fc05519175b5fda4392fc89e6c9c428adcceab
belfly1111 <belfly1111@naver.com>
2024-10-30 12:05:01 +0900

    [Add] Character & Hp

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public PlayerController controller;
+
+    private void Awake()
+    {
+        CharacterManager.Instance.Player = this;
+        controller = GetComponent<PlayerController>();
+    }
+}
